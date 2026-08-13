e7b1848b656fd7b51bd3ad81eecfd16a9d4eaad3
09anna <annayunarai09@gmail.com>
2026-08-13 16:48:20 +0545

    Stack Frontier

--- maze.py
+++ maze.py
@@ -10,7 +10,7 @@ class StackFrontier():
     def __init__(self):
         self.frontier=[] #Empty list
 
-    def add(self, node):
+    def add(self, node):  #adding somrthing to a frontier
         self.frontier.append(node)
 
     def contains_state(self, state): #checking whether a particular state already exits in frontier
@@ -18,3 +18,15 @@ class StackFrontier():
             if node.state==state:
                 return True
         return False
+
+    def empty(self): #check if the frontier is empty
+        return len(self.frontier)==0
+
+    def remove(self):  #removing from the frontier
+        if self.empty():
+            raise Exception("Empty Frontier")
+        else:
+            node= self.frontier[-1] #last item
+            self.frontier=self.frontier[:-1] 
+            return node
+
